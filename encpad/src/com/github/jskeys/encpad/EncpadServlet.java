ff89043147d14a7d2df1a8c0c88f0574dfe845e1
agea <agea@drwolf.it>
2010-12-07 18:26:24 +0100

    deleted old project with vaadin
    new project with jquery and servlets
    
    Signed-off-by: agea <agea@drwolf.it>

---
+++ encpad/src/com/github/jskeys/encpad/EncpadServlet.java
@@ -0,0 +1,13 @@
+package com.github.jskeys.encpad;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EncpadServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
