8b5bdbc8e73addb8d8d2cfe184a771d949b113b5
agea <agea@drwolf.it>
2010-12-21 15:25:44 +0100

    Signed-off-by: agea <agea@drwolf.it>

--- encpad/src/com/github/jskeys/encpad/EncpadServlet.java
+++ encpad/src/com/github/jskeys/encpad/EncpadServlet.java
@@ -1,6 +1,8 @@
 package com.github.jskeys.encpad;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +14,31 @@ public class EncpadServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		String action = req.getParameter("action");
-		if (action.equals("save")) {
-			this.save(req, resp);
+		try {
+			if (action.equals("save")) {
+				this.save(req, resp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	private void save(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
-
+	private void save(HttpServletRequest req, HttpServletResponse resp)
+			throws Exception {
+		String cypher = req.getParameter("save_cypher");
+		InputStream is = this.getClass().getResourceAsStream(
+				"savetemplate.html");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[8192];
+		int r;
+		while ((r = is.read(buffer)) != -1) {
+			baos.write(buffer, 0, r);
+		}
+		String html = new String(baos.toByteArray());
+		html = html.replaceAll("@@cypher@@", cypher);
+		resp.setHeader("Content-Disposition", "attachment");
+		resp.setHeader("Content-Type", "octet/stream");
+		resp.getWriter().write(html);
 	}
 
 }
