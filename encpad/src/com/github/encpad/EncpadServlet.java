5d3ffa690484fa9eba69b0fd4b1cfac439e034a9
agea <agea@drwolf.it>
2011-01-27 18:10:43 +0100

    Signed-off-by: agea <agea@drwolf.it>

--- encpad/src/com/github/encpad/EncpadServlet.java
+++ encpad/src/com/github/encpad/EncpadServlet.java
@@ -6,10 +6,13 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,20 +40,35 @@ public class EncpadServlet extends HttpServlet {
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 
-		String msgBody = "the body!";
+		String msgBody = "You can find your encrypted notes attached to this email.";
+
+		Multipart mp = new MimeMultipart();
+
+		MimeBodyPart txtPart = new MimeBodyPart();
+		txtPart.setContent(msgBody, "text/plain");
+		mp.addBodyPart(txtPart);
+
+		MimeBodyPart attachment = new MimeBodyPart();
+		attachment.setDisposition("attachment");
+		attachment.setFileName("encpad.html");
+		attachment.setContent(this.prepareHtml(req), "text/html");
+		mp.addBodyPart(attachment);
 
 		Message msg = new MimeMessage(session);
 		msg.setFrom(new InternetAddress("noreply@encpad.appspotmail.com"));
 		msg.addRecipient(Message.RecipientType.TO,
 				new InternetAddress(req.getParameter("save_email")));
-		msg.setSubject("the subject");
+		msg.setSubject("Your encrypted notes");
 		msg.setText(msgBody);
+
+		msg.setContent(mp);
+
 		Transport.send(msg);
+		resp.sendRedirect("/");
 
 	}
 
-	private void save(HttpServletRequest req, HttpServletResponse resp)
-			throws Exception {
+	private String prepareHtml(HttpServletRequest req) throws IOException {
 		String cypher = req.getParameter("save_cypher");
 		InputStream is = this.getClass().getResourceAsStream(
 				"savetemplate.html");
@@ -62,6 +80,12 @@ public class EncpadServlet extends HttpServlet {
 		}
 		String html = new String(baos.toByteArray());
 		html = html.replaceAll("@@cypher@@", cypher);
+		return html;
+	}
+
+	private void save(HttpServletRequest req, HttpServletResponse resp)
+			throws Exception {
+		String html = this.prepareHtml(req);
 		resp.setHeader("Content-Disposition", "attachment");
 		resp.setHeader("Content-Type", "octet/stream");
 		resp.getWriter().write(html);
