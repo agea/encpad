0caa5de98257e77f49aadab03a09c5daa251f324
agea <agea@drwolf.it>
2011-02-01 13:05:00 +0100

    Signed-off-by: agea <agea@drwolf.it>

--- encpad/src/com/github/encpad/EncpadServlet.java
+++ encpad/src/com/github/encpad/EncpadServlet.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
 import javax.mail.Message;
-import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -16,6 +18,8 @@ import javax.mail.internet.MimeMultipart;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.mail.ByteArrayDataSource;
 
 @SuppressWarnings("serial")
 public class EncpadServlet extends HttpServlet {
@@ -40,30 +44,33 @@ public class EncpadServlet extends HttpServlet {
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 
-		String msgBody = "You can find your encrypted notes attached to this email.";
-
-		Multipart mp = new MimeMultipart();
+		Message msg = new MimeMessage(session);
+		MimeMultipart mp = new MimeMultipart();
 
 		MimeBodyPart txtPart = new MimeBodyPart();
-		txtPart.setContent(msgBody, "text/plain");
+		txtPart.setContent(
+				"You can find your encrypted notes attached to this email.",
+				"text/plain");
 		mp.addBodyPart(txtPart);
 
-		MimeBodyPart attachment = new MimeBodyPart();
-		attachment.setDisposition("attachment");
-		attachment.setFileName("encpad.html");
-		attachment.setContent(this.prepareHtml(req), "text/html");
-		mp.addBodyPart(attachment);
-
-		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress("noreply@encpad.appspotmail.com"));
+		msg.setSubject("Your encrypted notes");
+		msg.setFrom(new InternetAddress("noreply@encpad.appspotmail.com",
+				"Encrypted Notepad"));
 		msg.addRecipient(Message.RecipientType.TO,
 				new InternetAddress(req.getParameter("save_email")));
-		msg.setSubject("Your encrypted notes");
-		msg.setText(msgBody);
+
+		MimeBodyPart attachmentPart = new MimeBodyPart();
+		attachmentPart.setFileName("encpad.html");
+		attachmentPart.setDisposition(Part.ATTACHMENT);
+		DataSource src = new ByteArrayDataSource(this.prepareHtml(req)
+				.getBytes(), "text/html");
+		DataHandler handler = new DataHandler(src);
+		attachmentPart.setDataHandler(handler);
+		mp.addBodyPart(attachmentPart);
 
 		msg.setContent(mp);
-
 		Transport.send(msg);
+
 		resp.sendRedirect("/");
 
 	}
